75f7d3e13f471a778fccccec8c2833b020232901
Umesh Verma <umeshjavastar@gmail.com>
2016-03-23 09:09:16 +0530

    Size Class done

---
+++ SizeClassDemoApp/SizeClassDemoApp/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  SizeClassDemoApp
+//
+//  Created by indianmesh on 2/18/16.
+//  Copyright © 2016 indianmesh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        print("Hi i am in view did load")
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
